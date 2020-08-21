9a3f1ec6f8f9574d47a88a165b35367bc5d2f054
Ryan Turner <rturner@twitter.com>
2020-08-20 17:24:50 -0700

    fix up doc str for methods

--- dict_minimize/tensorflow_api.py
+++ dict_minimize/tensorflow_api.py
@@ -58,24 +58,10 @@ def minimize(
       ub_dict (OrderedDict): Dictionary with same keys and shapes as `x0_dict` with upper bounds for \
         each variable. Set to `None` in an unconstrained problem.
       args (tuple): Extra arguments passed to the objective function.
-      method (str): Type of solver. Should be one of \
-          - 'Nelder-Mead' \
-          - 'Powell' \
-          - 'CG' \
-          - 'BFGS' \
-          - 'Newton-CG' \
-          - 'L-BFGS-B' \
-          - 'TNC' \
-          - 'COBYLA' \
-          - 'SLSQP' \
-          - 'trust-constr' \
-          - 'dogleg' \
-          - 'trust-ncg' \
-          - 'trust-exact' \
-          - 'trust-krylov' \
-          - custom - a callable object \
-        If not given, chosen to be one of ``BFGS``, ``L-BFGS-B``, ``SLSQP``, depending if the problem \
-        has constraints or bounds.
+      method (str): Type of solver. Should be one of: ``CG``, ``BFGS``, ``L-BFGS-B``, ``TNC``, \
+        ``SLSQP``, or ``trust-constr``. If not given, chosen to be one of ``BFGS``, ``L-BFGS-B``, \
+        ``SLSQP``, depending if the problem has bounds. Note, only ``L-BFGS-B``, ``TNC``, ``SLSQP`` \
+        seem to strictly respect the bounds ``lb_dict`` and ``ub_dict``.
       tol (float): Tolerance for termination. For detailed control, use solver-specific options.
       callback (callable): Called after each iteration. The signature is: \
           ``callback(xk)`` \
